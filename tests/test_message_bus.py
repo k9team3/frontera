e80b3020d9eee9c6f217a75fbc49d195a51a0f20
Alexander Sibiryakov <sixty-one@yandex.ru>
2016-07-25 23:33:48 +0200

    kafka mb test port from PR#136

--- tests/test_message_bus.py
+++ tests/test_message_bus.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 from frontera.settings import Settings
-from frontera.contrib.messagebus.zeromq import MessageBus
+from frontera.contrib.messagebus.zeromq import MessageBus as ZeroMQMessageBus
+from frontera.contrib.messagebus.kafkabus import MessageBus as KafkaMessageBus
 from frontera.utils.fingerprint import sha1
 from random import randint
 from time import sleep
 
 
 class MessageBusTester(object):
-    def __init__(self, settings=Settings()):
+    def __init__(self, cls, settings=Settings()):
         settings.set('SPIDER_FEED_PARTITIONS', 1)
         settings.set('QUEUE_HOSTNAME_PARTITIONING', True)
-        self.messagebus = MessageBus(settings)
+        self.messagebus = cls(settings)
         spiderlog = self.messagebus.spider_log()
 
         # sw
@@ -41,6 +42,7 @@ class MessageBusTester(object):
                 self.sp_sl_p.send(sha1(str(randint(1, 1000))), 'http://helloworld.com/way/to/the/sun/' + str(0))
             else:
                 self.sp_sl_p.send(sha1(str(randint(1, 1000))), 'http://way.to.the.sun' + str(0))
+        self.sp_sl_p.flush()
 
     def spider_feed_activity(self):
         sf_c = 0
@@ -50,10 +52,13 @@ class MessageBusTester(object):
 
     def sw_activity(self):
         c = 0
+        p = 0
         for m in self.sw_sl_c.get_messages(timeout=1.0, count=512):
             if m.startswith('http://helloworld.com/'):
+                p += 1
                 self.sw_us_p.send(None, 'message' + str(0) + "," + str(c))
             c += 1
+        assert p > 0
         return c
 
     def db_activity(self, messages):
@@ -70,7 +75,7 @@ class MessageBusTester(object):
                 self.db_sf_p.send("newhost", "http://newhost/new/url/to/crawl")
             else:
                 self.db_sf_p.send("someotherhost", "http://newhost223/new/url/to/crawl")
-
+        self.db_sf_p.flush()
         return (sl_c, us_c)
 
 
@@ -90,8 +95,29 @@ def test_zmq_message_bus():
     """
     Test MessageBus with default settings, IPv6 and Star as ZMQ_ADDRESS
     """
-    tester = MessageBusTester()
+    tester = MessageBusTester(ZeroMQMessageBus)
+    tester.spider_log_activity(64)
+    assert tester.sw_activity() == 64
+    assert tester.db_activity(128) == (64, 32)
+    assert tester.spider_feed_activity() == 128
 
+
+def test_kafka_message_bus_integration(self):
+    """
+    Test MessageBus with default settings, IPv6 and Star as ZMQ_ADDRESS
+    """
+    self.client.ensure_topic_exists("frontier-todo")
+    self.client.ensure_topic_exists("frontier-done")
+    self.client.ensure_topic_exists("frontier-score")
+
+    #logging.basicConfig(level=logging.INFO)
+    #kafkabus = logging.getLogger("kafkabus")
+    #kafkabus.addHandler(logging.StreamHandler())
+    settings = Settings()
+    settings.set('KAFKA_LOCATION', '%s:%s' % ("127.0.0.1", "9092"))
+    settings.set('FRONTIER_GROUP', 'frontier2')
+    settings.set('SCORING_TOPIC', "frontier-score")
+    tester = MessageBusTester(KafkaMessageBus, settings)
     tester.spider_log_activity(64)
     assert tester.sw_activity() == 64
     assert tester.db_activity(128) == (64, 32)
